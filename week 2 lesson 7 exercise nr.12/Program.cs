a1b9d7e4f2e2b2167b9d74deeeb974d54d3c81cf
konrad101296 <konradslimak@o2.pl>
2024-03-01 19:00:56 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.12/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("podaj dzien tygodnia:");
+int dzien = int.Parse(Console.ReadLine());
+
+if (dzien == 1)
+{
+    Console.WriteLine("poniedzialek");
+}
+else if (dzien == 2)
+{
+    Console.WriteLine("wtorek");
+}
+else if (dzien == 3) 
+{
+    Console.WriteLine("sroda");
+}
+else if (dzien == 4)
+{
+    Console.WriteLine("czwartek");
+}
+else if(dzien == 5)
+{
+    Console.WriteLine("piatek");
+}
+else if (dzien == 6)
+{
+    Console.WriteLine("sobota");
+}
+else if (dzien == 7)
+{
+    Console.WriteLine("niedziela");
+}
+else
+{
+    Console.WriteLine("podaj cyfre od 1 do 7");
+}
