ce91aa443bfbd3a9835ca14a72a3a2a8983e7e3e
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-06 17:03:12 +0100

    Update Program.cs
    dodano switch

--- week 2 lesson 7 exercise nr.12/Program.cs
+++ week 2 lesson 7 exercise nr.12/Program.cs
@@ -1,35 +1,63 @@
-﻿Console.WriteLine("podaj dzien tygodnia:");
-int dzien = int.Parse(Console.ReadLine());
+Console.WriteLine("podaj dzien tygodnia:");
+int day = int.Parse(Console.ReadLine());
 
-if (dzien == 1)
+if (day == 1)
 {
     Console.WriteLine("poniedzialek");
 }
-else if (dzien == 2)
+else if (day == 2)
 {
     Console.WriteLine("wtorek");
 }
-else if (dzien == 3) 
+else if (day == 3)
 {
     Console.WriteLine("sroda");
 }
-else if (dzien == 4)
+else if (day == 4)
 {
     Console.WriteLine("czwartek");
 }
-else if(dzien == 5)
+else if (day == 5)
 {
     Console.WriteLine("piatek");
 }
-else if (dzien == 6)
+else if (day == 6)
 {
     Console.WriteLine("sobota");
 }
-else if (dzien == 7)
+else if (day == 7)
 {
     Console.WriteLine("niedziela");
 }
 else
 {
     Console.WriteLine("podaj cyfre od 1 do 7");
+}
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("poniedzialek");
+        break;
+    case 2:
+        Console.WriteLine("wtorek");
+        break;
+    case 3:
+        Console.WriteLine("sroda");
+        break;
+    case 4:
+        Console.WriteLine("czwartek");
+        break;
+    case 5:
+        Console.WriteLine("piatek");
+        break;
+    case 6:
+        Console.WriteLine("sobota");
+        break;
+    case 7:
+        Console.WriteLine("niedziela");
+        break;
+    default:
+        Console.WriteLine("podaj cyfre od 1 do 7");
+        break;
 }
